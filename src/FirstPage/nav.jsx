d94d9efaeb4ce5d28167c438d825a2b93e11eb44
Shivam-018-hub <shivambharthi2000@gmail.com>
2026-07-20 01:00:23 +0530

    Update nav.jsx

--- src/FirstPage/nav.jsx
+++ src/FirstPage/nav.jsx
@@ -1,7 +1,17 @@
+import '../secondpage.css'
+import { Link,useLocation } from "react-router-dom";
 function Nav() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
     return (
         <div className="search-container">
+         
             <input type="text" className="search-box" placeholder="search your favourite Movies or Webseries..."/>
+             {!isHomePage &&
+                (<Link to="/" className="back-btn" >
+                    ⬅ Back
+                </Link>)
+            }
         </div>
 
     )
